491c1cf0e6f5e865ecd94f67e38ecd2552387839
Aierken Shalayiding <as4824@rit.edu>
2022-12-06 21:07:00 -0500

    63 done

--- 63.unique-paths-ii.cpp
+++ 63.unique-paths-ii.cpp
@@ -62,7 +62,42 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp (m, vector<int> (n,0));
+        bool x_one = true;
+        bool y_one = true;
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (i == 0 ){
+                    if (x_one == true)
+                    x_one = (x_one && obstacleGrid[i][j] == 1) ? false : true;
+                    dp [i][j] = x_one ? 1 : 0;
+                }else if (j == 0){
+                    if (y_one == true)
+                    y_one = (y_one && obstacleGrid[i][j] == 1) ? false : true;
+                    
+                    dp [i][j] = y_one ? 1 : 0;
+
+                }else {
+                    int obs = obstacleGrid[i][j] == 1 ? 0 : dp[i-1][j] + dp[i][j-1];
+                    dp[i][j] = obs;
+                }
+                // cout<<dp[i][j]<<" ";
+            }
+            // cout<<endl;
+        }
+
+
+
+
+
+
+
+        return dp[m-1][n-1];
+
     }
 };
 // @lc code=end
